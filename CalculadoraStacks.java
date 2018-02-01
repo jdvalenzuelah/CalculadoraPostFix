60682bd7e4ed23372b10b8a153ed16b8db70d088
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-02-01 14:24:24 -0600

    Operaciones y lectura de archivos

--- CalculadoraStacks.java
+++ CalculadoraStacks.java
@@ -7,45 +7,32 @@ public class CalculadoraStacks implements Calculadora{
 	@Override
 	public String calcular(String operacion) {
 		String[] splitOperacion = operacion.split(" ");
-		Integer resul = 0;
-		Integer temp1, temp2;
 		for( String charOperacion : splitOperacion) {
-			
-			switch(charOperacion) {
-			case "1":
-			case "2":
-			case "3":
-			case "4":
-			case "5":
-			case "6":
-			case "7":
-			case "8":
-			case "9":
-			case "0":
-				this.stack.push(Integer.parseInt(charOperacion));
-				break;
-			case "+":
-				int sizeSum = this.stack.size();
-				for(int i = 0; i < sizeSum; i++) {
-					resul = resul + this.stack.pop();
+			try {
+				stack.push(Integer.parseInt(charOperacion));
+			}catch(Exception e){
+				if(charOperacion.equals("+")) {
+					Integer resul = this.stack.pop();
+					resul += this.stack.pop();
+					this.stack.push(resul);
+				}else if(charOperacion.equals("-")) {
+					Integer resul = this.stack.pop();
+					resul -= this.stack.pop();
+					this.stack.push(resul);
+				}else if(charOperacion.equals("*")) {
+					Integer resul = this.stack.pop();
+					resul *= this.stack.pop();
+					this.stack.push(resul);
+				}else if(charOperacion.equals("/")) {
+					Integer resul = this.stack.pop();
+					resul /= this.stack.pop();
+					this.stack.push(resul);
 				}
-				this.stack.push(resul);
-				break;
-			case "*":
-				resul = 1;
-				int sizeMul = this.stack.size();
-				for(int i = 0; i < sizeMul; i++) {
-					resul = resul * this.stack.pop();
-				}
-				System.out.println(resul);
-				this.stack.push(resul);
-				
-				break;
 			}
 		
 		}
 			
-		return resul.toString();
+		return this.stack.peek().toString();
 	}
 	
 }
--- MainCalc.java
+++ MainCalc.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * 
  */
@@ -14,7 +20,14 @@ public class MainCalc {
 	 */
 	public static void main(String[] args) {
 		CalculadoraStacks calc = new CalculadoraStacks();
-		calc.calcular(" 1 2 + 4 * 2 *");	
+		File datos = new File("datos.txt");
+		try {
+			BufferedReader  reader = new BufferedReader(new FileReader(datos));
+			String line = reader.readLine();
+			System.out.println(line + " =" + calc.calcular(line));
+		}catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 
 	}
 
