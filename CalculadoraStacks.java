4be4fb37d44516edc2f5ae798d70ea3eac4a53d9
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-02-01 14:36:13 -0600

    .

--- CalculadoraStacks.java
+++ CalculadoraStacks.java
@@ -19,20 +19,6 @@ public class CalculadoraStacks implements Calculadora{
 					Integer resul = this.stack.pop();
 					resul -= this.stack.pop();
 					this.stack.push(resul);
-				}else if(charOperacion.equals("*")) {
-					Integer resul = this.stack.pop();
-					resul *= this.stack.pop();
-					this.stack.push(resul);
-				}else if(charOperacion.equals("/")) {
-					try{
-						Integer resul = this.stack.pop();
-						resul /= this.stack.pop();
-						this.stack.push(resul);
-					}catch(ArithmeticException er){
-						return("Error: "+ er.getMessage());
-					}
-				}else{
-					return "Error: Caracter incorrecto";
 				}
 			}
 		
